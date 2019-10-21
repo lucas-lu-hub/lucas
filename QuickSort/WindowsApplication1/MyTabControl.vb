3ebab8edbaa89859489304b9ec9ae6027ac0c713
Lu Yuanbo <Lucas.Lu@grapecity.com>
2019-10-21 17:06:40 +0900

    Delete All Trash

--- QuickSort/WindowsApplication1/MyTabControl.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class MyTabControl
-    Inherits TabControl
-    Protected Overrides Sub OnSelectedIndexChanged(e As EventArgs)
-
-    End Sub
-End Class
---
+++ TestProject/4.7.0/src/TestProject.Web/Models/Account/TenantChangeViewModel.cs
@@ -0,0 +1,11 @@
+using Abp.AutoMapper;
+using TestProject.Sessions.Dto;
+
+namespace TestProject.Web.Models.Account
+{
+    [AutoMapFrom(typeof(GetCurrentLoginInformationsOutput))]
+    public class TenantChangeViewModel
+    {
+        public TenantLoginInfoDto Tenant { get; set; }
+    }
+}
